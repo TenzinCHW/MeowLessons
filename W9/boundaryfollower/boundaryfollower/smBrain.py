1b3550ba7c4e7e278faa38d9e1697768289c4a91
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2016-04-09 19:47:03 +0800

    Used for testing for 2D in Week 11

--- W9/boundaryfollower/boundaryfollower/smBrain.py
+++ W9/boundaryfollower/boundaryfollower/smBrain.py
@@ -4,38 +4,46 @@ import libdw.sm as sm
 import libdw.gfx as gfx
 from soar.io import io
 
+time = 4.3
+
+
 # fin = open('C:\Users\HanWei\Dropbox\SUTDNotes\SUTDTerm3\DigitalWorld\W9\ebotcalib1.txt','w')
 class MySMClass(sm.SM):
     startState = 'Charge ahead'
 
     def getNextValues(self, state, inp):
-        left,fleft,front,fright,right,back,UK1,UK2 = inp.sonars
-        if state == 'Charge ahead':
-            if front <= 0.3 and fright > 0.3:
-                nextstate = 'Follow obstacle on right'
-                spd = [0,0.2]
-                # return nextstate,io.Action(fvel=0,rvel=0.2)
-            elif front > 0.3:
-                nextstate = state
-                spd = [0.1,0]
-                # return nextstate,io.Action(fvel=0.1,rvel=0)
-            return state,io.Action(fvel=spd[0],rvel=spd[1])
-        elif state == 'Follow obstacle on right':
-            if right >= 0.3:
-                nextstate = state
-                return nextstate,io.Action(fvel=0.1,rvel=-0.1)
-            elif right <= 0.2:
-                nextstate = 'Charge ahead'
-                return nextstate,io.Action(fvel=0.1,rvel=0)
-
-        # elif state == 'Follow closer':
-        #     if right <= 0.25:
+        # left,fleft,front,fright,right,back,UK1,UK2 = inp.sonars
+        # if state == 'Charge ahead':
+        #     if front <= 0.3 and fright > 0.3:
         #         nextstate = 'Follow obstacle on right'
-        #         return nextstate,io.Action(fvel=0.1,rvel=)
-        # fin.write(inp.sonars[1:4])
+        #         spd = [0,0.2]
+        #         # return nextstate,io.Action(fvel=0,rvel=0.2)
+        #     elif front > 0.3:
+        #         nextstate = state
+        #         spd = [0.1,0]
+        #         # return nextstate,io.Action(fvel=0.1,rvel=0)
+        #     return state,io.Action(fvel=spd[0],rvel=spd[1])
+        # elif state == 'Follow obstacle on right':
+        #     if right >= 0.3:
+        #         nextstate = state
+        #         return nextstate,io.Action(fvel=0.1,rvel=-0.1)
+        #     elif right <= 0.2:
+        #         nextstate = 'Charge ahead'
+        #         return nextstate,io.Action(fvel=0.1,rvel=0)
         print inp.sonars  # list
+        return state, io.Action(fvel=0,rvel=0)# state,io.Action(fvel=0.1,rvel=-0.02858)
         # print inp.odometry.theta
-        return (state, io.Action(fvel=0, rvel=0))
+
+
+        # global time
+        # if time > 0:
+        #     time -= 0.1
+        #     print time
+        #     return (state, io.Action(fvel=0,rvel=1))
+
+        # elif time > -8:
+        #     time -= 0.1
+        #     return (state, io.Action(fvel=0, rvel=0.5))
 
 
 mySM = MySMClass()
@@ -58,7 +66,6 @@ def plotSonar(sonarNum):
 def setup():
     robot.gfx = gfx.RobotGraphics(drawSlimeTrail=False,  # slime trails
                                   sonarMonitor=False)  # sonar monitor widget
-
     # set robot's behavior
     robot.behavior = mySM
 
