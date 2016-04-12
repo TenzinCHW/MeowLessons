8407696c3ae2d053cc02bfb5f328e1217fd091d2
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2016-04-13 00:48:03 +0800

    Week 12 checkoff

--- W12/templates/templates/kivycheckoffW12.py
+++ W12/templates/templates/kivycheckoffW12.py
@@ -4,13 +4,23 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+import RPi.GPIO as GPIO
+from time import sleep
+
+# Use the BCM GPIO numbers as the numbering scheme
+GPIO.setmode(GPIO.BCM)
+red = 5
+yellow = 16
+
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(yellow, GPIO.OUT)
 
 
 class Ledcontroller(App):
     def build(self):
         layout = GridLayout(cols=2)
         layout.add_widget(Label(text='Yellow LED'))
-        yellow = Button(text='off', on_press=self.LEDpress)
+        yellow = Button(text='off', id='yellow', on_press=self.LEDpress)
         layout.add_widget(yellow)
         layout.add_widget(Label(text='Red LED'))
         red = Button(text='off', on_press=self.LEDpress)
@@ -20,8 +30,17 @@ class Ledcontroller(App):
     def LEDpress(self, instance):
         if instance.text == 'off':
             instance.text = 'on'
+            if instance.id='yellow':
+                GPIO.output(yellow, GPIO.HIGH)
+            else:
+                GPIO.output(red, GPIO.HIGH)
+
         else:
-            instance.text = 'off'
+        instance.text = 'off'
+            if instance.id='yellow':
+                GPIO.output(yellow, GPIO.LOW)
+            else:
+                GPIO.output(red, GPIO.LOW)
 
 
 Ledcontroller().run()
